22352a3e6e8a2cb7b12d9d6aed8940274b8e6d4c
adel <adelabusaadya@ICLOUD.com>
2025-12-15 23:07:33 +0200

    التقرير

--- SallaAlertApp.Api/Migrations/20251215210553_ResolvePendingChanges.Designer.cs
+++ SallaAlertApp.Api/Migrations/20251215210553_ResolvePendingChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SallaAlertApp.Api.Data;
@@ -11,9 +12,11 @@ using SallaAlertApp.Api.Data;
 namespace SallaAlertApp.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215210553_ResolvePendingChanges")]
+    partial class ResolvePendingChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SallaAlertApp.Api/Migrations/20251215210553_ResolvePendingChanges.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SallaAlertApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResolvePendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastMonthlyReportSentAt",
+                table: "Subscriptions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastWeeklyReportSentAt",
+                table: "Subscriptions",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastMonthlyReportSentAt",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "LastWeeklyReportSentAt",
+                table: "Subscriptions");
+        }
+    }
+}
